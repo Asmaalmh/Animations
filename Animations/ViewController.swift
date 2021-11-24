53114231381d7727be0ee204d4bb933e24921894
Mobarak.Alseif@tuwaiq.edu.sa <Mobarak.Alseif@tuwaiq.edu.sa>
2021-11-24 10:54:01 +0300

    Done with Animations and Custom Control.

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -8,12 +8,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    fileprivate func Animations() {
+        UIView.animate(withDuration: 1, delay: 0.5, options: .curveEaseOut) {
+            self.imageView.transform = CGAffineTransform(rotationAngle: 120)
+        } completion: { _ in
+            UIView.animate(withDuration: 1, delay: 0.5, options: .curveEaseOut) {
+                self.imageView.alpha = 0
+            } completion: { _ in
+                
+            }
+            
+        }
+    }
+    
+    @IBAction func anmimate(_ sender: CustomControl) {
+        Animations()
+        sender.showLoading()
+    }
+    
 }
-
+class CustomControl : UIButton {
+    
+    private var originalButtonText: String?
+    var activityIndicator: UIActivityIndicatorView!
+    
+    func showLoading() {
+        originalButtonText = self.titleLabel?.text
+        self.setTitle("", for: .normal)
+        
+        if (activityIndicator == nil) {
+            activityIndicator = createActivityIndicator()
+        }
+        
+        showSpinning()
+    }
+    
+    func hideLoading() {
+        self.setTitle(originalButtonText, for: .normal)
+        activityIndicator.stopAnimating()
+    }
+    
+    private func createActivityIndicator() -> UIActivityIndicatorView {
+        let activityIndicator = UIActivityIndicatorView()
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.color = .lightGray
+        return activityIndicator
+    }
+    
+    private func showSpinning() {
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(activityIndicator)
+        centerActivityIndicatorInButton()
+        activityIndicator.startAnimating()
+    }
+    
+    private func centerActivityIndicatorInButton() {
+        let xCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: activityIndicator, attribute: .centerX, multiplier: 1, constant: 0)
+        self.addConstraint(xCenterConstraint)
+        
+        let yCenterConstraint = NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: activityIndicator, attribute: .centerY, multiplier: 1, constant: 0)
+        self.addConstraint(yCenterConstraint)
+    }
+}
